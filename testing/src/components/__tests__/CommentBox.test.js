d5cf46515377df6ed8c4ed20ee17d8cb606e5b85
brunobc <brunobezerrachaves@gmail.com>
2019-09-16 22:23:46 -0300

    Test submit textarea

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -23,5 +23,16 @@ it('has a text area that users can type in', () => {
     });
     wrapped.update();
 
-    
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
+
+it('should get emptied textarea after submitted', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'new comment' }
+    });
+    wrapped.update();
+
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
 });
