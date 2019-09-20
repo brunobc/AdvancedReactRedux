c5ce8e8135d62df4b51d3f667e8c44429093b796
brunobc <brunobezerrachaves@gmail.com>
2019-09-20 00:32:04 -0300

    Test comment action and reducer

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -10,9 +10,7 @@ class CommentBox extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-
         this.props.saveComment(this.state.comment);
-
         this.setState({ comment: ''});
     }
 
--- testing/src/index.js
+++ testing/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from 'reducers';
+import { Root } from 'Root';
 
 import App from 'components/App';
 
 ReactDOM.render(
-    <Provider store={createStore(reducers, {})}>
+    <Root>
         <App />
-    </Provider>
-    , document.getElementById('root'));
+    </Root>,
+    document.querySelector('#root')
+);
+ 
