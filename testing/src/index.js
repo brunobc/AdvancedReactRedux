43215beb014d9ac4feb46647f7e81fbc065d06aa
brunobc <brunobezerrachaves@gmail.com>
2019-09-25 00:15:25 -0300

    sign in

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { Root } from 'Root';
-
-import CommentBox from 'components/CommentBox';
-import CommentList from 'components/CommentList';
+import Root from 'Root';
+import App from 'components/App';
 
 ReactDOM.render(
     <Root>
         <BrowserRouter>
-            <Route path="/post" component={CommentBox} />
-            <Route path="/" exact component={CommentList} />
+            <Route path="/" component={App} />
         </BrowserRouter>
     </Root>,
     document.querySelector('#root')
