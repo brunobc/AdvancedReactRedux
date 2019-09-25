2d3109e45d5de189182a69af07c248b0afd9880b
brunobc <brunobezerrachaves@gmail.com>
2019-09-24 23:16:01 -0300

    Route comment box and list

--- testing/src/index.js
+++ testing/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Root } from 'Root';
 
-import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 ReactDOM.render(
     <Root>
         <BrowserRouter>
-            <Route path="/" component={App} />
+            <Route path="/post" component={CommentBox} />
+            <Route path="/" exact component={CommentList} />
         </BrowserRouter>
     </Root>,
     document.querySelector('#root')
