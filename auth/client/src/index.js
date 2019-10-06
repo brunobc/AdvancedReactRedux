1b26ae0929e45c5b99b8972e739480b763dcf0c8
brunobc <brunobezerrachaves@gmail.com>
2019-10-06 10:58:16 -0300

    auth

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -6,24 +6,32 @@ import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
-import App from './components/App'; 
+import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
+import Feature from './components/Feature';
+import Signout from './components/auth/Signout';
+import Signin from './components/auth/Signin';
 
 const store = createStore(
-    reducers,
-    {},
-    applyMiddleware(reduxThunk)
-)
+  reducers,
+  {
+    auth: { authenticated: localStorage.getItem('token') }
+  },
+  applyMiddleware(reduxThunk)
+);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App>
-                <Route path="/" exact component={Welcome} />
-                <Route path="/signup" component={Signup} />
-            </App>
-        </BrowserRouter>
-    </Provider>,
-    document.querySelector('#root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/feature" component={Feature} />
+        <Route path="/signout" component={Signout} />
+        <Route path="/signin" component={Signin} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('#root')
 );
